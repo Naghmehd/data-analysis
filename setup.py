7037c0b10b6bf93ae4bed5bd56937a267323bf3f
Naghmeh Shirazi <naghmehd@gmail.com>
2017-04-30 21:55:50 -0500

    added setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup (
+        name='data-analysis',
+        version='0.0.1',
+        url='www.github.com/naghmehd/data-analysis',
+        license='BSD',
+        author='Ben Hoff',
+        packages=find_packages(),
+        install_requires=['PyQt5',
+                          'pandas',
+                          'sqlalchemy',
+                          'nltk',
+                          'numpy',
+                          'jupyter', 'python-twitter'],
+        entry_points={},
+        extras_rquire={'dev': ['flake8',]},
+        )
