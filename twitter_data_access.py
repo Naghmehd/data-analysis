03c3c19f7f60e5589d29650b571b394aed46970f
Naghmeh Shirazi <naghmehd@gmail.com>
2017-05-01 20:38:12 -0500

    added twitter data access

---
+++ twitter_data_access.py
@@ -0,0 +1,36 @@
+from tweepy import OAuthHandler, Stream
+from tweepy.streaming import StreamListener
+
+consumer_key        = '1nvYyChmmQB3wXTPZ5MJDQ5qG'
+consumer_secret     = 'F8QeWpVHVO5k7jWJzZWQrxHY4ZVBqK28lLKnuLCmrX7hOhG1XZ'
+access_token      = 	'631410081-Mp1O4sKpkegQUCfElHA51B6i8e8qOG3ih5SgM0vk'
+access_token_secret = 	'hKjarfLPJraLTTyDXjhXPkF7sNO90TWnMpuubTvcmLxha'
+
+auth = OAuthHandler(consumer_key,
+                    consumer_secret)
+auth.set_access_token(access_token,
+                      access_token_secret)
+
+class PrintListener(StreamListener):
+    def on_status(self, status):
+        print(status.text)
+        print(status.author.screen_name,
+              status.created_at,
+              status.source,
+              '\n')
+
+    def on_error(self,status_code):
+        print("Error code: {}".format(status_code))
+        return True # keep stream alive
+
+    def on_timeout(self):
+        print('Listener timed out!')
+        return True # Keep stream alive
+
+def print_to_terminal():
+    listener = PrintListener()
+    stream = Stream(auth, listener)
+    stream.sample()
+
+if __name__ == '__main__':
+    print_to_terminal()
